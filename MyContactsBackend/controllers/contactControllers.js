231305fc4b722d9353518aa9542cf07dc452790a
srihanth-m <srishanthmule@gmail.com>
2024-04-13 09:43:02 +0530

    setup of error middleware

--- MyContactsBackend/controllers/contactControllers.js
+++ MyContactsBackend/controllers/contactControllers.js
@@ -3,32 +3,34 @@
 // the route
 // access
 
-const getContacts = (req, res) => {
+const asyncHandler = require("express-async-handler");
+
+const getContacts = asyncHandler(async (req, res) => {
   res.status(200).send("HEllo");
-};
+});
 
-const postContacts = (req, res) => {
-    console.log(req.body)
-    const {name , numbero , email} = req.body
-    console.log(name , numbero , email)
-    if(!name || !numbero || !email){
-        res.status(400);
-        throw new Error("name number and email should be compulasrily entered")
-    }
+const postContacts = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  const { name, numbero, email } = req.body;
+  console.log(name, numbero, email);
+  if (!name || !numbero || !email) {
+    res.status(400);
+    throw new Error("name number and email should be compulasrily entered");
+  }
   res.status(200).send("post contact");
-};
+});
 
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).send("get id'd contact");
-};
+});
 
-const updatecontact = (req, res) => {
+const updatecontact = asyncHandler(async (req, res) => {
   res.status(200).send("update id'd contact");
-};
+});
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).send("delete id'd contact");
-};
+});
 
 module.exports = {
   getContacts,
--- MyContactsBackend/server.js
+++ MyContactsBackend/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const contactRoute = require("./routes/contactsRoute");
+const errorHandler = require('./middlewares/errorHandler.js')
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use("/api/contacts", contactRoute);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`app listening at port ${port}`);
