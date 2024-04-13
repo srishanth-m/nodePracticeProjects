1ce9fdd933f43de8c3413e2d8a829999b9b47582
srihanth-m <srishanthmule@gmail.com>
2024-04-13 14:12:28 +0530

    modified something

--- MyContactsBackend/server.js
+++ MyContactsBackend/server.js
@@ -3,6 +3,7 @@ const app = express();
 require("dotenv").config();
 const contactRoute = require("./routes/contactsRoute");
 const errorHandler = require('./middlewares/errorHandler.js')
+const connectDb = require('./config/connect.js')
 
 const port = process.env.PORT || 5000;
 
@@ -11,5 +12,6 @@ app.use("/api/contacts", contactRoute);
 app.use(errorHandler);
 
 app.listen(port, () => {
+  connectDb()
   console.log(`app listening at port ${port}`);
 });
