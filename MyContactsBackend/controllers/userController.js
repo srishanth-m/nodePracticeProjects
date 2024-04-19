8ea3a3e57ab9ea8cff8c5808f2a449b6f71d24ef
srihanth-m <srishanthmule@gmail.com>
2024-04-19 10:25:28 +0530

    user api endpoints created

--- MyContactsBackend/controllers/userController.js
+++ MyContactsBackend/controllers/userController.js
@@ -1,15 +1,73 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userSchema.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("all fields are mandatory");
+  }
+
+  const userAvailable = User.findOne({ email });
+  if (!userAvailable) {
+    res.status(400);
+    throw new Error("user already registered");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log(hashedPassword);
+  const user = await User.create({
+    username,
+    email,
+    password,
+  });
+
+  console.log(`User created ${user}`);
+
+  if (user) {
+    res.status(200).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("user data is not valid");
+  }
+
   res.json({ message: "register the user here" });
 });
 
 const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("all fields are mandatory");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("email or password is not valid");
+  }
+
   res.json({ message: "login user" });
 });
 
-const currentUser = asyncHandler(async(req , res) =>{
-    res.json({message : "current user"})
-})
+const currentUser = asyncHandler(async (req, res) => {
+  res.json({ message: "current user" });
+});
 
 module.exports = { registerUser, loginUser, currentUser };
